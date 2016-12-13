050f2be1bad9f83e9986b88f84781068ade0e6a1
James Davis <james.davisphd@gmail.com>
2016-12-12 23:44:40 -0600

    playing with pending vs subscribed setting in the helper

--- app/helpers/mailchimp_helper.rb
+++ app/helpers/mailchimp_helper.rb
@@ -3,5 +3,8 @@ require 'gibbon'
 module MailchimpHelper
   def self.add_to_list(user)
     Gibbon::Request.lists(ENV['LIST_ID']).members.create(body: {email_address: user.email, status: "pending", merge_fields: {FNAME: "First Name", LNAME: "Last Name"}})
+    #gibbon = Gibbon::Request.new
+    #gibbon.api_endpoint = "https://us13.api.mailchimp.com/"
+    #gibbon.lists(ENV['LIST_ID']).members.create(body: {email_address: user.email, status: "pending", merge_fields: {FNAME: "First Name", LNAME: "Last Name"}})
   end
 end
